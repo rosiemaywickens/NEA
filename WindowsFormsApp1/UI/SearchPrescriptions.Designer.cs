f99cad3ba5505dd40b6733325a6c2c2a886e885e
Rosie Wickens <rosiemaywickensuk@icloud.com>
2024-02-16 16:44:39 +0000

    j

--- WindowsFormsApp1/UI/SearchPrescriptions.Designer.cs
+++ WindowsFormsApp1/UI/SearchPrescriptions.Designer.cs
@@ -28,31 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.surnamecheck = new System.Windows.Forms.CheckBox();
+            this.prenum = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelsearch = new System.Windows.Forms.Label();
+            this.searchbox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // listBox1
+            // surnamecheck
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(569, 382);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(629, 479);
-            this.listBox1.TabIndex = 0;
+            this.surnamecheck.AutoSize = true;
+            this.surnamecheck.Location = new System.Drawing.Point(91, 419);
+            this.surnamecheck.Name = "surnamecheck";
+            this.surnamecheck.Size = new System.Drawing.Size(156, 29);
+            this.surnamecheck.TabIndex = 0;
+            this.surnamecheck.Text = "by surname";
+            this.surnamecheck.UseVisualStyleBackColor = true;
+            this.surnamecheck.CheckedChanged += new System.EventHandler(this.surnamecheck_CheckedChanged);
+            // 
+            // prenum
+            // 
+            this.prenum.AutoSize = true;
+            this.prenum.Location = new System.Drawing.Point(91, 454);
+            this.prenum.Name = "prenum";
+            this.prenum.Size = new System.Drawing.Size(269, 29);
+            this.prenum.TabIndex = 1;
+            this.prenum.Text = "by prescription number:";
+            this.prenum.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // labelsearch
+            // 
+            this.labelsearch.AutoSize = true;
+            this.labelsearch.Location = new System.Drawing.Point(86, 379);
+            this.labelsearch.Name = "labelsearch";
+            this.labelsearch.Size = new System.Drawing.Size(284, 25);
+            this.labelsearch.TabIndex = 3;
+            this.labelsearch.Text = "search for prescriptiopns by:";
+            // 
+            // searchbox
+            // 
+            this.searchbox.Location = new System.Drawing.Point(91, 500);
+            this.searchbox.Name = "searchbox";
+            this.searchbox.Size = new System.Drawing.Size(235, 31);
+            this.searchbox.TabIndex = 4;
             // 
             // SearchPrescriptions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.searchbox);
+            this.Controls.Add(this.labelsearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prenum);
+            this.Controls.Add(this.surnamecheck);
             this.Name = "SearchPrescriptions";
             this.Size = new System.Drawing.Size(1504, 1064);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox surnamecheck;
+        private System.Windows.Forms.CheckBox prenum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelsearch;
+        private System.Windows.Forms.TextBox searchbox;
     }
 }
--- WindowsFormsApp1/UI/SearchPrescriptions.cs
+++ WindowsFormsApp1/UI/SearchPrescriptions.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1.UI
         {
             InitializeComponent();
         }
+
+        private void surnamecheck_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
